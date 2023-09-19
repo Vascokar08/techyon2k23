82f638b26c8e9c56fb7e3e8b80ecee04062e5a39
SHIVAM KORGAONKAR <115925079+Vascokar08@users.noreply.github.com>
2023-09-19 13:29:54 +0530

    Add files via upload

---
+++ techyon2k23_2/techyon2k23_2/cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.3.5.
+ * Original file: /gh/alpinejs/alpine@null/dist/alpine.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
